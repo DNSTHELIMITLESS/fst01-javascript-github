3dd1800876a0bd88f73daa7a2145e83255eb0014
DNSTHELIMITLESS <matheusmartinho1905@gmail.com>
2024-03-26 22:22:09 -0300

    aula js intro

---
+++ aula2-operadores/operadoresMat.js
@@ -0,0 +1,20 @@
+let numero1 = 6;
+let numero2 = 2;
+
+console.log(" ");
+console.log(" --------- ");
+console.log(`numero 1: ${numero1}`);
+console.log(`numero 2: ${numero2}`);
+console.log(" --------- ");
+console.log(" ");
+
+// operadores
+
+// soma
+
+let soma = numero1 + numero2; // operação matematica
+console.log("A soma é: " + soma); // concatenação
+
+// modulo
+
+let modulo = numero1 % numero2;
